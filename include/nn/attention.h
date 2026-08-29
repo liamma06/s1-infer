6a25bf8462e39f4160a738fc30bf89973074d6fa
liamma06 <liam.jbr.ma@gmail.com>
2026-08-28 21:27:10 -0400

    attention quantized replicate

--- include/nn/attention.h
+++ include/nn/attention.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "infer/kv_block_pool.h"
 #include "core/tensor.h"
+#include "core/quantized_tensor.h"
 
 TensorPtr reshape_to_heads(const TensorPtr& input, size_t num_heads, size_t head_dim);
 
@@ -16,5 +17,18 @@ TensorPtr self_attention(
     const TensorPtr& q_norm_weight,  
     const TensorPtr& k_norm_weight,  
     KVBlockPool& cache,
-    size_t sequence_id                 
+    size_t sequence_id
+);
+
+// same but int* quantized weights 
+TensorPtr self_attention_quantized(
+    const TensorPtr& x,
+    const QuantizedTensor& q_proj_weight,
+    const QuantizedTensor& k_proj_weight,
+    const QuantizedTensor& v_proj_weight,
+    const QuantizedTensor& o_proj_weight,
+    const TensorPtr& q_norm_weight,
+    const TensorPtr& k_norm_weight,
+    KVBlockPool& cache,
+    size_t sequence_id
 );
--- src/nn/attention.cpp
+++ src/nn/attention.cpp
@@ -1,8 +1,8 @@
 #include "nn/attention.h"
-#include <cmath>
-#include <limits>
 #include "nn/rmsnorm.h"
 #include "nn/RoPE.h"
+#include <cmath>
+#include <limits>
 
 TensorPtr reshape_to_heads(const TensorPtr& input, size_t num_heads, size_t head_dim) {
     size_t seq_len = input->shape()[0];
@@ -122,6 +122,52 @@ TensorPtr self_attention(
 
     //linear proj
     auto output = attn_out_flat->matmul(o_proj_weight);
+
+    return output;
+}
+
+TensorPtr self_attention_quantized(
+    const TensorPtr& x,
+    const QuantizedTensor& q_proj_weight,
+    const QuantizedTensor& k_proj_weight,
+    const QuantizedTensor& v_proj_weight,
+    const QuantizedTensor& o_proj_weight,
+    const TensorPtr& q_norm_weight,
+    const TensorPtr& k_norm_weight,
+    KVBlockPool& cache,
+    size_t sequence_id
+){
+    size_t curr_pos = cache.length(sequence_id);
+
+    TensorPtr Q_flat, K_flat, V_flat;
+    Q_flat = matmul_quantized(x, q_proj_weight);
+    K_flat = matmul_quantized(x, k_proj_weight);
+    V_flat = matmul_quantized(x, v_proj_weight);
+
+    //{tensor, head, dim}
+    auto Q_heads = reshape_to_heads(Q_flat, 16, 128);
+    auto K_heads = reshape_to_heads(K_flat, 8, 128);
+    auto V_heads = reshape_to_heads(V_flat, 8, 128);
+
+    Q_heads = rmsnorm(Q_heads, q_norm_weight, 1e-6f);
+    Q_heads = apply_rope(Q_heads, curr_pos);
+
+    K_heads = rmsnorm(K_heads, k_norm_weight, 1e-6f);
+    K_heads = apply_rope(K_heads, curr_pos);
+
+    cache.append(sequence_id, K_heads, V_heads);
+
+    TensorPtr full_k = cache.get_k(sequence_id);
+    TensorPtr full_v = cache.get_v(sequence_id);
+
+    auto attn_out = GQA_attention(Q_heads, full_k, full_v, 16, 8, 128);
+
+    size_t seq_len = x->shape()[0];
+
+    auto attn_out_flat = attn_out->reshape({seq_len, 16 * 128});
+
+    //linear proj
+    auto output = matmul_quantized(attn_out_flat, o_proj_weight);
 
     return output;
 }
